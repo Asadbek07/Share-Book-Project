4535cf977272fbd222f4cbd6fe008bc733093f9a
Asadbek <asindarov07@gmail.com>
2022-03-15 21:30:10 +0500

    configured static files for DEBUG mode

--- BookExchangeProject/settings.py
+++ BookExchangeProject/settings.py
@@ -114,13 +114,16 @@ USE_I18N = True
 USE_TZ = True
 
 
-MEDIA_ROOT = "media"
+MEDIA_ROOT = BASE_DIR/"media"
 MEDIA_URL = "/media/"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
