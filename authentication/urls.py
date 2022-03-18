9081ad82ae2a1776380b230b34c97e45fd5522ae
Asadbek <asindarov07@gmail.com>
2022-03-18 08:59:14 +0500

    Added user detail page

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from authentication.views import AuthUserDetailView, SignupPageView
+from authentication.views import AuthUserDetailView, SignupPageView, UserDetailPage
 
 urlpatterns = [
-    path("<pk>/update", AuthUserDetailView.as_view(), name="user_detail_update"),
+    path("<int:pk>/update", AuthUserDetailView.as_view(), name="user_detail_update"),
+    path("<int:pk>/", UserDetailPage.as_view(), name="user_detail"),
     path("", SignupPageView.as_view(), name="signup"),
 
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,9 @@
 from typing import Any, Dict
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView 
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from authentication.forms import CustomUserCreationForm
 from .models import AuthUser
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
@@ -35,4 +35,10 @@ class HomePageView(TemplateView):
     template_name = "authentication/home.html"
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super(HomePageView, self).get_context_data(**kwargs)
-        return context                    
+        return context     
+
+class UserDetailPage(LoginRequiredMixin, DetailView):
+    model = AuthUser
+    context_object_name = "profile"
+    template_name = "authentication/profile/detail.html"
+    login_url = "login"             
